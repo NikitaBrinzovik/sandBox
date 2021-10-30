237fd39f18f1b273996c1382d9d9ba85982fd0e7
Nikita <knanaprimer@gmail.com>
2021-10-30 22:14:21 +0300

    init: none feat: move logic to js from hardcode(html) fix: none

--- Slider/index.js
+++ Slider/index.js
@@ -1,11 +1,17 @@
-const slides = document.querySelectorAll(".slide")
+const slidesPlugin = (activeSlide = 2) => {
+    const slides = document.querySelectorAll(".slide")
 
-for (const slide of slides) {
-    slide.addEventListener("click", ()=> {
-        clearActiveClasses()
-        slide.classList.add("active")
-    })
+    slides[activeSlide].classList.add("active")
+
+    for (const slide of slides) {
+        slide.addEventListener("click", ()=> {
+            clearActiveClasses()
+            slide.classList.add("active")
+        })
+    }
+    function clearActiveClasses () {
+        slides.forEach(slide=> slide.classList.remove("active"))
+    }
 }
-function clearActiveClasses () {
-    slides.forEach(slide=> slide.classList.remove("active"))
-}
+
+slidesPlugin()
